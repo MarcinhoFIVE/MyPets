d2a22069ca538429a8d8814170b854b7b337bec1
MarcinhoFIVE <darlan_lifecat@yahoo.com.br>
2022-03-30 20:16:39 -0300

    Novo icone e introducao de novo layout /Input Text & Button

--- app/src/main/java/br/com/etecia/mypets/LoginActivity.java
+++ app/src/main/java/br/com/etecia/mypets/LoginActivity.java
@@ -3,6 +3,8 @@ package br.com.etecia.mypets;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -12,4 +14,7 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.login_layout);
     }
 
+    public void AcessaSistema(View view) {
+        Toast.makeText(getApplicationContext(), "Welcome to the jungle", Toast.LENGTH_SHORT).show();
+    }
 }
