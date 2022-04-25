86b61d826c3481396e054632ca3dd14e5507573e
MarcinhoFIVE <darlan_lifecat@yahoo.com.br>
2022-04-25 20:21:41 -0300

    Click Exit Button

--- app/src/main/java/br/com/etecia/mypets/LoginActivity.java
+++ app/src/main/java/br/com/etecia/mypets/LoginActivity.java
@@ -4,17 +4,36 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+    EditText edtUsuario, edtSenha;
+    Button btnEntrar, btnSair;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
+        edtUsuario = findViewById(R.id.etUser);
+        edtSenha = findViewById(R.id.etPass);
+        btnEntrar = findViewById(R.id.btnEntrar);
+        btnSair = findViewById(R.id.btnSair);
+
+        //Evento de click nos botões
+
+        btnSair.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+
     }
 
     public void AcessaSistema(View view) {
-        Toast.makeText(getApplicationContext(), "Welcome to the jungle", Toast.LENGTH_SHORT).show();
+
     }
 }
